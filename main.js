9ffb694f1bc971fd3831ae231b1eb95ec9653424
Samuel <samuelzeleke24@yahoo.com>
2018-10-20 14:01:32 -0400

    added pop up windows

---
+++ main.js
@@ -0,0 +1,33 @@
+let age;
+let acolor="red";
+console.log("age is currently;+age")
+age=26;
+console.log("age is currently;+age")
+age=26+1;
+console.log("hello world!")
+if (5>4){
+
+}console.log("5 is greater than 4")
+console.log("5 is equal to 4")
+
+if ((3>(3-1))&&(acolor!="silver")&&(acolor=="re" + "d"))
+{
+    console.log("did we make it?");
+
+}
+
+let pro = prompt("what is your name?");
+let userName = confirm("my nam is samuel");
+
+let username2=prompt("what is your name");
+
+let nameConfirm = confirm("just checking, is" + username2 + " your real name?");
+
+if(nameConfirm){
+    alert("wellcom to my page, " + username2 + "!"); 
+}
+    else {
+        username2 = prompt("okay,what is your name?");
+    }
+    console.log("the users name is " + username2 + ".")
+    
